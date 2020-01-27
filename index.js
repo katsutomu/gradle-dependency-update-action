e8f15e62c9fb42c2efdd1d0ebfd08dacf442a811
katsutomu <katsutomu0124@gmail.com>
2020-01-28 01:28:02 +0900

    Create pull request with changed gradle file

--- index.js
+++ index.js
@@ -1,38 +1,67 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
+// const fs = require('fs');
 const reporter = require('./reporter');
 
 async function run() {
   try {
-    const fileValue = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/app/build/dependencyUpdates/report.json`, 'utf8');
-    const replaced = reporter.generateReplace(`${process.env.GITHUB_WORKSPACE}/app/build.gradle`, reporter.parse(JSON.parse(fileValue)))
     const fullRepository = process.env.GITHUB_REPOSITORY.split('/');
     const owner = fullRepository[0];
     const repo = fullRepository[1];
 
+    console.log("start update");
+    let files = await reporter.loadGradleFiles()
+    files = files.map(function(file){
+        file.path = file.path.replace(`${process.env.GITHUB_WORKSPACE}/`, '')
+        return file
+      }
+    )
+    console.log(files);
+    const paths = files.map(file => file.path)
     const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
-    await octokit.git.createRef({
-      owner: owner,
-      repo: repo,
-      ref: 'refs/heads/dependencies-update',
-      sha: process.env.GITHUB_SHA,
-    })
-    const content = await octokit.repos.getContents({
-      owner: owner,
-      repo: repo,
-      ref: 'refs/heads/dependencies-update',
-      path: 'app/build.gradle', // ファイルパス
-    })
-    await octokit.repos.createOrUpdateFile({
-      owner: owner,
-      repo: repo,
-      branch: 'dependencies-update',
-      message: 'update dependencies',
-      path: 'app/build.gradle',
-      content: new Buffer(`${ replaced }`).toString('base64'), // base64にエンコードする
-      sha: content.data.sha,
-    })
+    const preFiles = await reporter.fetchRemoteFiles(octokit, paths, process.env.GITHUB_REF, owner, repo)
+    console.log(preFiles);
+    const diffFiles = []
+    files.forEach(file => {
+      let matchedFile = preFiles.find(preFile => preFile.path == file.path)
+      if (
+        matchedFile != undefined &&
+        matchedFile.content != file.content
+      ) {
+        file.presha = matchedFile.sha
+        diffFiles.push(file)
+      }
+    });
+    console.log(diffFiles);
+    if (diffFiles.length) {
+      await octokit.git.createRef({
+        owner: owner,
+        repo: repo,
+        ref: 'refs/heads/dependencies-update',
+        sha: process.env.GITHUB_SHA,
+      })
+      const requests = diffFiles.map(async function(item){
+        await octokit.repos.createOrUpdateFile({
+          owner: owner,
+          repo: repo,
+          branch: 'dependencies-update',
+          message: `update dependencies ${item.path}`,
+          path: item.path,
+          content: item.content,
+          sha: item.presha,
+        })
+      });
+      for await (const result of requests) {
+        console.log(result);
+      }
+      await octokit.pulls.create({
+        owner: owner,
+        repo: repo,
+        title: 'Dependencies Update',
+        head: 'dependencies-update',
+        base: 'develop'
+      })
+    }
   }
   catch (error) {
     core.setFailed(error.message);
--- reporter.js
+++ reporter.js
@@ -1,6 +1,44 @@
 const fs = require('fs')
+const glob = require('@actions/glob');
+const path = require('path');
 
 var reporter = {};
+
+reporter.loadGradleFiles = async function() {
+  const patterns = ['**/*.gradle', '**/*.kts', '!**/.gradle/']
+  const globber = await glob.create(patterns.join('\n'))
+  const files = await globber.glob()
+  return files.map(function(filePath){
+    const fileValue = fs.readFileSync(filePath, 'utf8')
+    return {
+      path: filePath,
+      content: new Buffer(fileValue).toString('base64')
+    }
+  })
+}
+
+reporter.fetchRemoteFiles = async function(octokit, filePaths, ref, owner, repo) {
+  const requests = filePaths.map(async function(path){
+    const content = await octokit.repos.getContents({
+      owner: owner,
+      repo: repo,
+      ref: ref,
+      path: path,
+    })
+    return {
+      path: path,
+      content: content.data.content.replace(/\r?\n/g, ''),
+      sha: content.data.sha
+    }
+  })
+  var results = [];
+  for await (const result of requests) {
+    console.log(result);
+    results.push(result);
+  }
+
+  return results
+}
 
 reporter.parse = function(dependencies) {
   if (typeof(dependencies) !== 'object') {
